a0bf84ce6c3965c8fa2c01f1360fb64feefcea9b
Sumaiya Siddika <to.sumaiyasiddika@gmail.com>
2023-03-27 11:51:21 +0600

    update distignore

--- dist/index.asset.php
+++ dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '79662d6bd74db9b09071');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f3e474d4159de8e51987');
